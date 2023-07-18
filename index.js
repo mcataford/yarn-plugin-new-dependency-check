73184494f1536bfcacaca8e35b143a096313f898
Marc Cataford <mcat@riseup.net>
2023-07-18 19:17:07 -0400

    wip: find new dependencies via double-resolution and compare approach

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ function isNoColourMode() {
 	return Boolean(process.env.NO_COLOR)
 }
 
-function getPrompt(descriptorsLength) {
+function getPrompt(packagesToInstall) {
 	const dependencyCount = isNoColourMode()
-		? `About to install ${descriptorsLength} new packages.\n`
-		: `About to install \x1b[1;34m${descriptorsLength}\x1b[1;0m new packages.\n`
+		? `About to install ${packagesToInstall.length} new packages.\n`
+		: `About to install \x1b[1;34m${packagesToInstall.length}\x1b[1;0m new packages.\n`
 
 	const question = isNoColourMode()
 		? 'Continue? (Y/Yes to continue, anything else to cancel): '
@@ -19,6 +19,7 @@ function getPrompt(descriptorsLength) {
 	return `${'='.repeat(10)}
     ${dependencyCount}
     ${dependencyInfo}
+    Packages: ${packagesToInstall.join(', ')}
 ${'='.repeat(10)}
 ${question}`
 }
@@ -29,8 +30,10 @@ const CANCELLATION_MESSAGE = isNoColourMode()
 
 const RESOLVE_MESSAGE =
 	'📦 Resolving direct and transitive dependencies being added...\nThis may take longer if the packages added have a lot of dependencies.'
+
 const UNKNOWN_ERROR_MESSAGE =
 	'🤔 Failed to match some dependencies while mapping new packages, results may be inaccurate.'
+
 /*
  *  Builds a collection of new dependencies based on resolved packages and known added
  *  package names.
@@ -80,10 +83,7 @@ module.exports = {
 		const { ThrowReport } = require('@yarnpkg/core')
 		const readline = require('readline')
 
-		const state = {
-			addedTopLevelPackages: undefined,
-			addedIdentHashes: new Set(),
-		}
+		let addedPackages
 
 		return {
 			hooks: {
@@ -98,38 +98,18 @@ module.exports = {
 				 * See hook documentation: https://yarnpkg.com/advanced/plugin-tutorial#hook-afterWorkspaceDependencyAddition
 				 */
 				async afterWorkspaceDependencyAddition(workspace, target, descriptor, strategies) {
-					/*
-					 * This hook is executed for each added dependency such that
-					 * `yarn add A B C` will call it thrice. To avoid this, we can
-					 * parse the command-line arguments to determine what the user is
-					 * trying to add.
-					 *
-					 * Because the hook is run multiple times, we cache the result of the
-					 * arg analysis to avoid redoing it each time.
-					 */
-					state.addedTopLevelPackages =
-						state.addedTopLevelPackages ||
-						process.argv.filter((arg, position) => {
-							// The first three arguments are the node and yarn runtime paths, and
-							// the add command.
-							if (position < 3) return false
-
-							// Flags passed to `yarn add` are ignored.
-							if (arg.startsWith('-')) return false
-
-							return true
-						})
-
-					// Keeping track of descriptor hashes added directly speeds up the work
-					// of building the dependency trees later.
-					state.addedIdentHashes.add(descriptor.identHash)
-
-					// To run this logic once, we wait until the last item is being processed.
-					if (state.addedTopLevelPackages[state.addedTopLevelPackages.length - 1] !== descriptor.name) {
-						return
-					}
+					if (addedPackages) return
 
 					process.stdout.write(`${RESOLVE_MESSAGE}\n`)
+
+					await workspace.project.resolveEverything({
+						// FIXME: Will fail when used with unpublished tarballs.
+						lockfileOnly: true,
+						// TODO: Reporting boilerplate.
+						report: new ThrowReport(),
+					})
+
+					const beforePackageLocators = new Set([...workspace.project.storedPackages.keys()])
 
 					await workspace.project.resolveEverything({
 						// FIXME: Will fail when used with unpublished tarballs.
@@ -138,18 +118,22 @@ module.exports = {
 						report: new ThrowReport(),
 					})
 
-					const newDependencies = determineNewDependencies(
-						workspace.project.storedPackages,
-						workspace.project.storedDescriptors,
-						state.addedIdentHashes,
-					)
+					const newDependencies = [...workspace.project.storedPackages.entries()]
+						.filter(([key]) => {
+							return !beforePackageLocators.has(key)
+						})
+						.map(([, packageData]) => {
+							return `${packageData.name}@${packageData.version}`
+						})
+
+					addedPackages = newDependencies
 
 					const rl = readline.createInterface({
 						input: process.stdin,
 						output: process.stdout,
 					})
 
-					const userInput = await new Promise((resolve) => rl.question(getPrompt(newDependencies.size), resolve))
+					const userInput = await new Promise((resolve) => rl.question(getPrompt(newDependencies), resolve))
 
 					rl.close()
 
